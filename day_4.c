024062489e4719180be5ae8d81b6f7caf604023b
mayankramola88061-wq <mayankramola88061@gmail.com>
2026-02-10 19:29:07 +0530

    Create day_4.c

---
+++ day_4.c
@@ -0,0 +1,40 @@
+//Reverse an Array In-Place
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int left = 0, right = n - 1;
+    while (left < right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+//remove element 
+int removeElement(int* nums, int numsSize, int val) {
+  int k = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;  
+}
